d7e48d82ae088f202516b7a379d7dc83fa8e6e3d
MarkLombardiNelson <marklombardinelson@gmail.com>
2017-02-07 11:39:33 -0500

    Hacking current_user to default to the first user until auth0 works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::API
   include Knock::Authenticable
 
   def current_user
-    @current_user ||= token ? User.authenticate(token) : nil
+    # Temp using the first user as the current user until auth0 works
+    User.first
+
+    # @current_user ||= token ? User.authenticate(token) : nil
   end
 end
